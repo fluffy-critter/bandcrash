572cc2d764ef54bd8e1897b17086ccf6c34b8772
fluffy <fluffy@beesbuzz.biz>
2023-10-06 17:23:48 -0700

    Fancy locking was causing a deadlock somewhere, let's just not pool the base image

--- bandcrash/images.py
+++ bandcrash/images.py
@@ -4,35 +4,31 @@ import functools
 import io
 import os.path
 import threading
-import typing
 
 import PIL.Image
 
 from .util import slugify_filename
 
-
-@functools.lru_cache()
-def load_image(in_path: str) -> typing.Tuple[threading.Lock, PIL.Image]:
+def load_image(in_path: str) -> PIL.Image:
     """ Load an image into memory, pooling it """
-    return threading.Lock(), fix_orientation(PIL.Image.open(in_path))
+    return PIL.Image.open(in_path)
 
 
 @functools.lru_cache()
-def generate_image(in_path: str, size: int) -> typing.Tuple[threading.Lock, PIL.Image]:
+def generate_image(in_path: str, size: int) -> PIL.Image:
     """ Given an image path, generate a rendition that fits within the size constraint
 
     :param str in_path: Path to the file
     :param int size: Maximum size (both width and height)
     """
-    lock, image = load_image(in_path)
-    with lock:
-        out_w = int(min(image.width*size/image.height, size))
-        out_h = int(min(image.height*size/image.width, size))
-        if out_w > image.width or out_h > image.height:
-            out_w = image.width
-            out_h = image.height
+    image = load_image(in_path)
+    out_w = int(min(image.width*size/image.height, size))
+    out_h = int(min(image.height*size/image.width, size))
+    if out_w > image.width or out_h > image.height:
+        out_w = image.width
+        out_h = image.height
 
-        return lock, image.resize(size=(out_w, out_h), resample=PIL.Image.Resampling.LANCZOS)
+    return image.resize(size=(out_w, out_h), resample=PIL.Image.Resampling.LANCZOS)
 
 
 @functools.lru_cache()
@@ -46,11 +42,10 @@ def generate_rendition(in_path: str, out_dir: str, size: int) -> str:
     :returns: a file path
     """
 
-    lock, image = generate_image(in_path, size)
+    image = generate_image(in_path, size)
     basename, _ = os.path.splitext(os.path.basename(in_path))
     out_file = slugify_filename(f'{basename}.{size}.jpg')
-    with lock:
-        image.convert('RGB').save(os.path.join(out_dir, out_file))
+    image.convert('RGB').save(os.path.join(out_dir, out_file))
 
     return out_file
 
@@ -66,9 +61,7 @@ def generate_blob(in_path: str, size: int, ext: str = "jpeg") -> bytes:
     :returns: In-memory compressed file
     """
     buffer = io.BytesIO()
-    lock, image = generate_image(in_path, size)
-    with lock:
-        image.convert('RGB').save(buffer, format=ext)
+    generate_image(in_path, size).convert('RGB').save(buffer, format=ext)
     return buffer.getvalue()
 
 
