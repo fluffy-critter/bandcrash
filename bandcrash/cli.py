3fc09a22003c9bd2c4a38ab4e31052a4945f383f
fluffy <fluffy@beesbuzz.biz>
2023-10-09 22:38:26 -0700

    Some more refactoring

--- bandcrash/cli.py
+++ bandcrash/cli.py
@@ -7,16 +7,16 @@ import json
 import logging
 import os
 
-from . import parse_args, populate_json_file, process
+from . import args, populate_json_file, process
 
 LOG_LEVELS = [logging.WARNING, logging.INFO, logging.DEBUG]
 LOGGER = logging.getLogger(__name__)
 
 
-def main(args=None):
+def main():
     """ Main entry point """
     # pylint:disable=too-many-branches,too-many-statements,too-many-locals
-    options = parse_args(False, args)
+    options = args.parse_args()
 
     logging.basicConfig(level=LOG_LEVELS[min(
         options.verbosity, len(LOG_LEVELS) - 1)],
@@ -25,11 +25,14 @@ def main(args=None):
     json_path = os.path.join(options.input_dir, options.json)
 
     if options.init:
+        LOGGER.info("Creating a default configuration in %s", json_path)
         album = populate_json_file(options.input_dir, json_path)
         if not options.output_dir:
             return
 
-    options = parse_args(True)
+    if not options.output_dir:
+        raise ValueError("Missing output directory")
+
     with open(json_path, 'r', encoding='utf8') as json_file:
         album = json.load(json_file)
 
