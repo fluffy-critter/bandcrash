370d1f4d64a11f5b0728df1ff1e905d8148f05aa
fluffy <fluffy@beesbuzz.biz>
2023-10-13 11:12:22 -0700

    The extremely simple rudiments of the album form editor

--- bandcrash/gui.py
+++ bandcrash/gui.py
@@ -1,171 +1,116 @@
 """ Bandcrash GUI """
 # pylint:disable=invalid-name,too-few-public-methods,too-many-ancestors
-import wx
-import wx.adv
+import argparse
+import json
+import logging
 
-from . import __version__, args
+from PySide6 import QtCore, QtGui, QtWidgets
+
+from bandcrash import __version__, args
+
+LOG_LEVELS = [logging.WARNING, logging.INFO, logging.DEBUG]
+LOGGER = logging.getLogger(__name__)
 
 
-class AboutBox(wx.adv.AboutDialogInfo):
-    """ Simple about box """
-
-    def __init__(self, parent):
+class AlbumEditor(QtWidgets.QWidget):
+    def __init__(self, load_file=None):
         super().__init__()
 
+        # album metadata
+        self.album = {'tracks':[]}
 
-class Preferences(wx.Dialog):
-    """ preferences panel """
+        # json key -> widget
+        self.connections = {}
 
-    def __init__(self, parent):
-        wx.Dialog.__init__(self, parent, -1, "Preferences")
+        layout = QtWidgets.QFormLayout(fieldGrowthPolicy=QtWidgets.QFormLayout.AllNonFixedFieldsGrow)
+        self.setLayout(layout)
 
-    @staticmethod
-    def LoadConfig(reset=False):
-        """ Load or reset the application configuration """
-        cfg = wx.ConfigBase.Get()
+        artist_name = QtWidgets.QLineEdit(placeholderText = "Artist name")
+        self.connect(artist_name, artist_name.textChanged, 'artist')
+        layout.addRow("Artist", artist_name)
 
-        defaults = vars(args.config_parser().parse_args())
-        for key, val in defaults.items():
-            if reset or not cfg.Exists(key):
-                if isinstance(val, str):
-                    cfg.Write(key, val)
-                elif isinstance(val, int):
-                    cfg.WriteInt(key, val)
-                elif isinstance(val, bool):
-                    cfg.WriteBool(key, val)
-                else:
-                    raise RuntimeError(
-                        "Got unexpected type %s for key %s", type(defaults[item]), item)
+        album_title = QtWidgets.QLineEdit(placeholderText = "Album title")
+        self.connect(album_title, album_title.textChanged, 'title')
+        layout.addRow("Album", album_title)
 
-        cfg.Flush()
+        release_year = QtWidgets.QLineEdit(inputMask='0000', placeholderText = "1978")
+        self.connect(release_year, release_year.textChanged, 'year', int)
+        layout.addRow("Year", release_year)
 
-    @staticmethod
-    def ApplyConfig(options : argparse.Namespace):
-        """ Apply the application configuration to an option set """
-        cfg = wx.ConfigBase.Get()
+        album_genre = QtWidgets.QLineEdit(placeholderText = "Avant-Industrial Loungecore")
+        self.connect(album_genre, album_genre.textChanged, 'genre')
+        layout.addRow("Genre", album_genre)
 
-        # oh my god why is wxPython's config API so stupid
-        more, key, idx = cfg.GetFirstEntry()
-        while more:
-            if hasattr(options, key):
-                cfg_type = cfg.GetEntryType(key)
-                if cfg_type == wx.ConfigBase.EntryType.Type_String:
-                    setattr(options, key, cfg.Read(key))
-                elif cfg_type == wx.ConfigBase.EntryType.Type_Integer:
-                    setattr(options, key, cfg.ReadInt(key))
-                elif cfg_type == wx.ConfigBase.EntryType.Type_Boolean:
-                    setattr(options, key, cfg.ReadBool(key))
-                else:
-                    raise RuntimeError(
-                        "Got unexpected type %d for key %s", cfg_type, key)
+        # TODO: player colors
+        # TODO: album artwork
 
-        for key in vars(config).keys():
-            if cfg.Exists(key):
+        start_button = QtWidgets.QPushButton("Go!")
+        start_button.clicked.connect(self.encode_album)
+        layout.addRow(start_button)
 
+    def connect(self, widget, signal, key, transform=lambda x:x):
+        """ Connect a widget signal to an album metadata field
 
+        :param widget: The owning widget
+        :param signal: The signal slot to connect
+        :param str key: The JSON key to set
+        :param transform: Optional function for data conversion
+        """
 
-class AlbumEditor(wx.Frame):
-    """ Album editor frame """
+        self.connections[key] = widget
+        def apply(value):
+            if value:
+                self.album[key] = value
+            else:
+                del self.album[key]
 
-    def __init__(self, parent, open_file=None):
-        wx.Frame.__init__(self, parent, -1, "New Album")
+            if key in ('artist', 'title'):
+                self.updateDisplayTitle()
 
-        self.album = None  # Currently open album specification
-        self.unsaved = False  # Are there unsaved changes?
+        signal.connect(apply)
 
-        self.SetupMenu()
+    def updateDisplayTitle(self):
+        """ Update the album's display title """
+        parts = []
+        if "artist" in self.album:
+            parts.append(self.album["artist"])
+        if "title" in self.album:
+            parts.append(self.album["title"])
+        if parts:
+            self.output_name = ' - '.join(parts)
+        else:
+            self.output_name = "(untitled album)"
 
-        self.Bind(wx.EVT_MENU, self.OpenPreferences, id=wx.ID_PREFERENCES)
-        self.Bind(wx.EVT_MENU, self.ShowAbout, id=wx.ID_ABOUT)
-        self.Bind(wx.EVT_MENU, self.OpenDialog, id=wx.ID_OPEN)
+        self.setWindowTitle(self.output_name)
 
-    def SetupMenu(self):
-        menu_bar = wx.MenuBar()
-
-        menu = wx.Menu()
-        menu.Append(wx.ID_NEW, "&New\tCtrl-N", "Create a new album")
-        menu.Append(wx.ID_OPEN, "&Open...\tCtrl-O", "Open an existing album")
-        menu.Append(wx.ID_SAVE, "&Save\tCtrl-S", "Save the current album")
-        menu.Append(wx.ID_SAVEAS, "Save &As...\tCtrl-Shift-S",
-                    "Save to a new filename")
-        menu.Append(wx.ID_REVERT_TO_SAVED, "&Revert", "Undo all changes")
-        menu.AppendSeparator()
-        menu.Append(wx.ID_EXIT, "&Quit\tCtrl-Q", "Quit the app")
-        menu_bar.Append(menu, "&File")
-
-        menu = wx.Menu()
-        menu.Append(wx.ID_PREFERENCES, "&Preferences",
-                    "Open application preferences")
-        menu_bar.Append(menu, "&Edit")
-
-        menu = wx.Menu()
-        menu.Append(wx.ID_ABOUT, "&About...", "About Bandcrash")
-        menu_bar.Append(menu, "&Help")
-
-        wx.MenuBar.MacSetCommonMenuBar(menu_bar)
-        self.SetMenuBar(menu_bar)
-
-    def OpenPreferences(self, event):
-        """ Open the preferences dialog """
-        with Preferences(self) as dlg:
-            dlg.ShowModal()
-
-    def OpenDialog(self, event):
-        """ Open a file through a dialog """
-
-    def ShowAbout(self, event):
-        """ Show the about box """
-        info = wx.adv.AboutDialogInfo()
-        info.SetName("Bandcrash")
-        info.SetWebSite("https://github.com/fluffy-critter/bandcrash",
-                        "Github Repository")
-        info.SetVersion(__version__.__version__)
-        info.SetCopyright("© 2022—2023 j. \"fluffy\" shagam")
-
-        info.SetLicense("""MIT License
-
-Copyright (c) 2022—2023 j. shagam <fluffy@beesbuzz.biz>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-""")
-        wx.adv.AboutBox(info)
-
-
-class BandcrashApp(wx.App):
-    """ app instance """
-
-    def OnInit(self):
-        """ set up the app """
-        self.SetAppName("Bandcrash")
-        self.SetVendorName("biz.beesbuzz.bandcrash")
-
-        Preferences.LoadConfig()
-
-        frame = AlbumEditor(None)
-        frame.Show(True)
-        self.SetTopWindow(frame)
-        return True
-
+    def encode_album(self):
+        """ Run the encoder process """
+        print(self.album)
 
 def main():
     """ instantiate an app """
-    app = BandcrashApp()
-    app.MainLoop()
+    parser = argparse.ArgumentParser(description="GUI for Bandcrash")
+    parser.add_argument('-v', '--verbosity', action="count",
+                        help="increase output verbosity", default=0)
+    parser.add_argument('--version', action='version',
+                        version="%(prog)s " + __version__.__version__)
+    parser.add_argument('open_files', type=str, nargs='*')
+    options = parser.parse_args()
+
+    logging.basicConfig(level=LOG_LEVELS[min(
+        options.verbosity, len(LOG_LEVELS) - 1)],
+        format='%(message)s')
+
+    LOGGER.debug(
+        "Opening bandcrash GUI with provided files: [%s]", options.open_files)
+
+    app = QtWidgets.QApplication([])
+
+    mainwin = AlbumEditor()
+    mainwin.show()
+
+    app.exec()
+
 
 
 if __name__ == '__main__':
