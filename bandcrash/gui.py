440ecbe5300735733f6053d063976fffbde31fab
fluffy <fluffy@beesbuzz.biz>
2023-10-14 02:11:07 -0700

    I was overcomplicating things, let's just do things the naive way

--- bandcrash/gui.py
+++ bandcrash/gui.py
@@ -1,5 +1,6 @@
 """ Bandcrash GUI """
 # pylint:disable=invalid-name,too-few-public-methods,too-many-ancestors
+# type: ignore
 import argparse
 import json
 import logging
@@ -14,85 +15,51 @@ LOG_LEVELS = [logging.WARNING, logging.INFO, logging.DEBUG]
 LOGGER = logging.getLogger(__name__)
 
 
-class EditorBinding:
-    """ Maintain bindings between editor and JSON data
-
-    :param dict data: The dict to bind the editor to
-    """
-
-    def __init__(self, data):
-        self.data = data
-        self.setters: typing.Dict[str,
-                                  typing.Tuple[typing.Callable, typing.Callable]] = {}
-
-    def connect(self, key, signal, setter, to_json=lambda x: x, to_qt=lambda x: x):
-        """ Connect a widget signal to a metadata field
-
-        :param str key: The key in the dict
-        :param signal: The editor signal slot
-        :param setter: The widget's setter method
-        :param to_json: Type transform when setting the JSON field
-        :param to_qt: Type transform when setting the Qt field
-        """
-        def apply(value):
-            if value:
-                self.data[key] = to_json(value)
-            else:
-                del self.data[key]
-
-        signal.connect(apply)
-
-        self.setters[key] = (setter, to_qt)
-
-    def apply(self):
-        """ Apply the dict's data to the editor """
-        for key, (setter, xform) in self.setters.items():
-            if key in self.data:
-                setter(xform(self.data[key]))
-
-
+def to_checkstate(val):
+    """ Convert a bool to a qt CheckState """
+    return QtCore.Qt.Checked if val else QtCore.Qt.Unchecked
 
 class FileSelector(QtWidgets.QWidget):
     """ A file selector textbox with ... button """
 
-    @typing.no_type_check
-    def __init__(self, dialog_text="Choose file",
-        filters:typing.Optional[typing.List[str]]=None):
+    def __init__(self):
         super().__init__()
 
         layout = QtWidgets.QHBoxLayout()
         self.setLayout(layout)
 
-        self.path_text = QtWidgets.QLineEdit()
+        self.file_path = QtWidgets.QLineEdit()
         self.button = QtWidgets.QPushButton("...")
 
-        self.dialog_text = dialog_text
-        self.filters = filters
-
-        layout.addWidget(self.path_text)
+        layout.addWidget(self.file_path)
         layout.addWidget(self.button)
 
         self.button.clicked.connect(self.choose_file)
 
     def choose_file(self):
         """ Pick a file """
-        dialog = QtWidgets.QFileDialog(caption=self.dialog_text)
-        if self.filters:
-            dialog.setNameFilters(filters)
-        (filename, _) = dialog.getOpenFileName()
+        dialog = QtWidgets.QFileDialog()
+        (filename, _) = dialog.getOpenFileName(self)
         if filename:
-            self.path_text.setText(filename)
+            self.file_path.setText(filename)
 
     @staticmethod
     def make_relative(base_file):
-        """ Returns a function to provide a path relative to the specified filename """
-        return lambda path : os.path.relpath(path, os.basename(base_file))
+        """ Returns a function to provide a path relative to the specified filename or directory """
+        if os.path.isdir(base_file):
+            dirname = base_file
+        else:
+            dirname = os.path.dirname(base_file)
+
+        return lambda path: (os.path.relpath(path, dirname)
+                             if os.path.isabs(path)
+                             else path)
+
 
 class TrackEditor(QtWidgets.QWidget):
     """ A track editor pane """
 
-    @typing.no_type_check
-    def __init__(self, album_editor, data:dict):
+    def __init__(self, album_editor, data: dict):
         """ edit an individual track
 
         :param dict data: The metadata blob
@@ -103,45 +70,110 @@ class TrackEditor(QtWidgets.QWidget):
         self.album_editor = album_editor
         self.data = data
 
-        self.binding = EditorBinding(self.data)
-
         layout = QtWidgets.QFormLayout(
             fieldGrowthPolicy=QtWidgets.QFormLayout.AllNonFixedFieldsGrow)
         self.setLayout(layout)
 
-        widget = FileSelector("Choose audio file")
-        self.binding.connect('filename', widget.path_text.textChanged,
-            widget.path_text.setText, FileSelector.make_relative(self.album_editor.filename))
-        layout.addRow("Audio file", widget)
+        self.filename = FileSelector()
+        self.title = QtWidgets.QLineEdit(placeholderText="Song title")
+        self.genre = QtWidgets.QLineEdit()
+        self.artist = QtWidgets.QLineEdit(
+            placeholderText="Track-specific artist (leave blank if none)")
+        self.cover_of = QtWidgets.QLineEdit(
+            placeholderText="Original performing artist (leave blank if none)")
+        self.artwork = FileSelector()
+        self.lyrics = QtWidgets.QPlainTextEdit()
 
-        # TODO track artwork
+        self.preview = QtWidgets.QCheckBox("Generate preview")
+        self.hidden = QtWidgets.QCheckBox("Hidden track")
 
-        widget = QtWidgets.QLineEdit(placeholderText="Song title")
-        self.binding.connect('title', widget.textChanged, widget.setText)
-        layout.addRow("Title", widget)
+        layout.addRow("Audio file", self.filename)
+        layout.addRow("Title", self.title)
+        layout.addRow("Genre", self.genre)
+        layout.addRow("Track artist", self.artist)
+        layout.addRow("Original performer", self.cover_of)
+        layout.addRow("Artwork", self.artwork)
+        layout.addRow("Lyrics", self.lyrics)
 
-        widget = QtWidgets.QLineEdit()
-        self.binding.connect('genre', widget.textChanged, widget.setText)
-        layout.addRow("Genre", widget)
+        player_options = QtWidgets.QHBoxLayout()
+        player_options.addWidget(self.preview)
+        player_options.addWidget(self.hidden)
+        layout.addRow(player_options)
 
-        widget = QtWidgets.QLineEdit(placeholderText="Track-specific artist")
-        self.binding.connect('artist', widget.textChanged, widget.setText)
-        layout.addRow("Performing Artist", widget)
+        buttons = QtWidgets.QHBoxLayout()
 
-        lyrics = QtWidgets.QTextEdit()
-        self.binding.connect('lyrics', lyrics.textChanged, lyrics.setText,
-            ,
-            '\n'.join)
-        layout.addRow("Lyrics", widget)
+        apply_button = QtWidgets.QPushButton("Apply")
+        apply_button.clicked.connect(self.apply)
+        buttons.addWidget(apply_button)
 
-        self.binding.apply()
+        reset_button = QtWidgets.QPushButton("Reset")
+        reset_button.clicked.connect(self.reset)
+        buttons.addWidget(reset_button)
 
+        layout.addRow(buttons)
+
+        self.reset()
+
+    def reset(self):
+        """ Reset to the backing data """
+        for key, widget in (
+            ('filename', self.filename.file_path),
+            ('title', self.title),
+            ('genre', self.genre),
+            ('artist', self.artist),
+            ('cover_of', self.cover_of),
+            ('artwork', self.artwork.file_path),
+        ):
+            widget.setText(self.data.get(key, ''))
+
+        self.lyrics.document().setPlainText('\n'.join(self.data.get('lyrics', '')))
+
+        self.preview.setCheckState(self.data.get('preview', to_checkstate(True)))
+        self.hidden.setCheckState(self.data.get('hidden', to_checkstate(False)))
+
+    def apply(self):
+        """ Apply our data to the backing data """
+
+        relpath = FileSelector.make_relative(self.album_editor.filename)
+
+        for key, widget in (
+            ('filename', self.filename.file_path),
+            ('artwork', self.artwork.file_path),
+        ):
+            if value := widget.text():
+                self.data[key] = relpath(value)
+            elif key in self.data:
+                del self.data[key]
+
+        for key, widget in (
+            ('title', self.title),
+            ('genre', self.genre),
+            ('artist', self.artist),
+            ('cover_of', self.cover_of),
+        ):
+            if value := widget.text():
+                self.data[key] = value
+            elif key in self.data:
+                del self.data[key]
+
+        if value := self.lyrics.document().toPlainText():
+            lines = value.split('\n')
+            self.data['lyrics'] = lines if len(lines) != 1 else lines[0]
+        else:
+            del self.data['lyrics']
+
+        for key, widget, dfl in (('preview', self.preview, True),
+                                 ('hidden', self.hidden, False)):
+            value = widget.checkState() == QtCore.Qt.Checked
+            if value != dfl:
+                self.data[key] = value
+            elif key in self.data:
+                del self.data[key]
 
 
 class AlbumEditor(QtWidgets.QWidget):
     """ An album editor window """
 
-    @typing.no_type_check
     def __init__(self, path: str):
         """ edit an album file
 
@@ -150,70 +182,107 @@ class AlbumEditor(QtWidgets.QWidget):
         super().__init__()
         self.setMinimumSize(600, 0)
 
-        self.filename: str = path
-        self.album: typing.Dict[str, typing.Any] = {'tracks': []}
+        self.filename = path
+        self.data: typing.Dict[str, typing.Any] = {'tracks': []}
         if os.path.isfile(path):
             with open(path, 'r', encoding='utf8') as file:
-                self.album = json.load(file)
-
-        self.binding = EditorBinding(self.album)
+                self.data = json.load(file)
 
         layout = QtWidgets.QFormLayout(
             fieldGrowthPolicy=QtWidgets.QFormLayout.AllNonFixedFieldsGrow)
         self.setLayout(layout)
 
-        text = QtWidgets.QLineEdit(placeholderText="Artist name")
-        self.binding.connect('artist', text.textChanged, text.setText)
-        text.textChanged.connect(self.updateDisplayTitle)
-        layout.addRow("Artist", text)
+        self.artist = QtWidgets.QLineEdit(placeholderText="Artist name")
+        self.title = QtWidgets.QLineEdit(placeholderText="Album title")
+        self.year = QtWidgets.QLineEdit(
+            placeholderText="1978", inputMask='0000')
+        self.genre = QtWidgets.QLineEdit(
+            placeholderText="Avant-Industrial Loungecore")
+        self.artwork = FileSelector()
+        # self.fg_color = ColorSelector("Foreground")
+        # self.bg_color = ColorSelector("Background")
+        # self.highlight_color = ColorSelector("Highlight")
 
-        text = QtWidgets.QLineEdit(placeholderText="Album title")
-        self.binding.connect('title', text.textChanged, text.setText)
-        text.textChanged.connect(self.updateDisplayTitle)
-        layout.addRow("Album", text)
+        layout.addRow("Artist", self.artist)
+        layout.addRow("Title", self.title)
+        layout.addRow("Year", self.year)
+        layout.addRow("Genre", self.genre)
+        layout.addRow("Artwork", self.artwork)
 
-        text = QtWidgets.QLineEdit(
-            inputMask='0000', placeholderText="1978")
-        self.binding.connect('year', text.textChanged, text.setText, int, str)
-        layout.addRow("Year", text)
-
-        text = QtWidgets.QLineEdit()
-        self.binding.connect('genre', text.textChanged, text.setText)
-        layout.addRow("Genre", text)
-
-        # TODO: player colors
-        # TODO: album artwork
+        # button hbox for colors
 
         # TODO this needs to be a track list box on the left
-        for track in self.album['tracks']:
-            layout.addRow(track.get('title', 'no title'), TrackEditor(self, track))
+        for track in self.data['tracks']:
+            layout.addRow(track.get('title', 'no title'),
+                          TrackEditor(self, track))
             break
 
-        start_button = QtWidgets.QPushButton("Go!")
+        buttons = QtWidgets.QHBoxLayout()
+
+        save_button = QtWidgets.QPushButton("Save")
+        save_button.clicked.connect(self.apply)
+        start_button = QtWidgets.QPushButton("Encode")
         start_button.clicked.connect(self.encode_album)
-        layout.addRow(start_button)
 
-        self.binding.apply()
-        self.updateDisplayTitle()
+        buttons.addWidget(save_button)
+        buttons.addWidget(start_button)
 
-    def updateDisplayTitle(self):
-        """ Update the album's display title """
+        layout.addRow(buttons)
 
-        title = os.path.basename(self.filename)
+        self.setWindowTitle(self.filename)
 
-        parts = []
-        if "artist" in self.album:
-            parts.append(self.album["artist"])
-        if "title" in self.album:
-            parts.append(self.album["title"])
-        if parts:
-            title += f": {' — '.join(parts)}"
+        self.reset()
 
-        self.setWindowTitle(title)
+    def reset(self):
+        """ Reset to the saved values """
+
+        for key, widget in (
+            ('artist', self.artist),
+            ('title', self.title),
+            ('genre', self.genre),
+            ('artwork', self.artwork.file_path),
+        ):
+            widget.setText(self.data.get(key, ''))
+
+        if 'year' in self.data:
+            self.year.setText(str(self.data['year']))
+
+    def apply(self):
+        """ Apply edits to the saved data """
+        relpath = FileSelector.make_relative(self.filename)
+
+        for key, widget in (
+            ('title', self.title),
+            ('genre', self.genre),
+            ('artist', self.artist),
+        ):
+            if value := widget.text():
+                self.data[key] = value
+            elif key in self.data:
+                del self.data[key]
+
+        for key, widget in (
+            ('artwork', self.artwork.file_path),
+        ):
+            if value := widget.text():
+                self.data[key] = relpath(value)
+            elif key in self.data:
+                del self.data[key]
+
+        if value := self.year.text():
+            self.data['year'] = int(value)
+        elif 'year' in self.data:
+            del self.data['year']
+
+        # TODO apply all track editors as well
+
+        # with open(self.filename, 'w', encoding='utf8') as file:
+        #     json.dump(self.data, file, indent='   ')
 
     def encode_album(self):
         """ Run the encoder process """
-        print(json.dumps(self.album, indent='   '))
+        self.apply()
+        print(json.dumps(self.data, indent='   '))
 
 
 def main():
