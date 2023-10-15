536d06d462ca36c39e3e294a7cafda7aa99e5ba4
fluffy <fluffy@beesbuzz.biz>
2023-10-15 01:30:05 -0700

    Add a bunch of menu actions and such, but menus aren't appearing???

--- bandcrash/gui.py
+++ bandcrash/gui.py
@@ -9,7 +9,7 @@ import typing
 
 from PySide6 import QtCore, QtGui, QtWidgets
 
-from bandcrash import __version__, args
+from . import __version__, args, util
 
 LOG_LEVELS = [logging.WARNING, logging.INFO, logging.DEBUG]
 LOGGER = logging.getLogger(__name__)
@@ -19,6 +19,7 @@ def to_checkstate(val):
     """ Convert a bool to a qt CheckState """
     return QtCore.Qt.Checked if val else QtCore.Qt.Unchecked
 
+
 class FileSelector(QtWidgets.QWidget):
     """ A file selector textbox with ... button """
 
@@ -27,7 +28,7 @@ class FileSelector(QtWidgets.QWidget):
 
         layout = QtWidgets.QHBoxLayout()
         # layout.setSpacing(0)
-        layout.setContentsMargins(0,0,0,0)
+        layout.setContentsMargins(0, 0, 0, 0)
 
         self.setLayout(layout)
 
@@ -46,18 +47,6 @@ class FileSelector(QtWidgets.QWidget):
         if filename:
             self.file_path.setText(filename)
 
-    @staticmethod
-    def make_relative(base_file):
-        """ Returns a function to provide a path relative to the specified filename or directory """
-        if os.path.isdir(base_file):
-            dirname = base_file
-        else:
-            dirname = os.path.dirname(base_file)
-
-        return lambda path: (os.path.relpath(path, dirname)
-                             if os.path.isabs(path)
-                             else path)
-
 
 class TrackEditor(QtWidgets.QWidget):
     """ A track editor pane """
@@ -71,7 +60,7 @@ class TrackEditor(QtWidgets.QWidget):
         self.setMinimumSize(400, 0)
 
         self.album_editor = album_editor
-        self.data : typing.Optional[typing.Dict[str, typing.Any]] = None
+        self.data: typing.Optional[typing.Dict[str, typing.Any]] = None
 
         layout = QtWidgets.QFormLayout(
             fieldGrowthPolicy=QtWidgets.QFormLayout.AllNonFixedFieldsGrow)
@@ -135,8 +124,10 @@ class TrackEditor(QtWidgets.QWidget):
         else:
             self.lyrics.document().setPlainText('\n'.join(lyrics))
 
-        self.preview.setCheckState(to_checkstate(self.data.get('preview', True)))
-        self.hidden.setCheckState(to_checkstate(self.data.get('hidden', False)))
+        self.preview.setCheckState(
+            to_checkstate(self.data.get('preview', True)))
+        self.hidden.setCheckState(
+            to_checkstate(self.data.get('hidden', False)))
 
     def apply(self):
         """ Apply our data to the backing data """
@@ -144,7 +135,7 @@ class TrackEditor(QtWidgets.QWidget):
         if not self.data:
             return
 
-        relpath = FileSelector.make_relative(self.album_editor.filename)
+        relpath = util.make_relative_path(self.album_editor.filename)
 
         for key, widget in (
             ('filename', self.filename.file_path),
@@ -171,7 +162,7 @@ class TrackEditor(QtWidgets.QWidget):
 
         for key, widget in (
             ('lyrics', self.lyrics),
-            ):
+        ):
             if value := widget.document().toPlainText():
                 lines = value.split('\n')
                 self.data[key] = lines if len(lines) != 1 else lines[0]
@@ -186,15 +177,41 @@ class TrackEditor(QtWidgets.QWidget):
             elif key in self.data:
                 del self.data[key]
 
+
 class TrackListing(QtWidgets.QSplitter):
     """ The track listing panel and editor """
+
     def __init__(self, album_editor):
         super().__init__()
 
         self.data = album_editor.data['tracks']
 
+        left_panel = QtWidgets.QVBoxLayout(self)
+        left_panel.setSpacing(0)
+        left_panel.setContentsMargins(0, 0, 0, 0)
+        self.addWidget(QtWidgets.QWidget(layout=left_panel))
+
         self.track_listing = QtWidgets.QListWidget(self)
-        self.addWidget(self.track_listing)
+        left_panel.addWidget(self.track_listing)
+
+        self.button_add = QtWidgets.QPushButton("+")
+        self.button_add.clicked.connect(self.add_tracks)
+        self.button_delete = QtWidgets.QPushButton("-")
+        self.button_delete.clicked.connect(self.delete_track)
+        self.button_move_up = QtWidgets.QPushButton("^")
+        self.button_move_up.clicked.connect(self.move_up)
+        self.button_move_down = QtWidgets.QPushButton("v")
+        self.button_move_down.clicked.connect(self.move_down)
+
+        buttons = QtWidgets.QHBoxLayout(self)
+        buttons.setSpacing(0)
+        buttons.setContentsMargins(0, 0, 0, 0)
+        buttons.addWidget(self.button_add)
+        buttons.addWidget(self.button_delete)
+        buttons.addStretch(1000)
+        buttons.addWidget(self.button_move_up)
+        buttons.addWidget(self.button_move_down)
+        left_panel.addWidget(QtWidgets.QWidget(layout=buttons))
 
         self.track_editor = TrackEditor(album_editor)
         scroller = QtWidgets.QScrollArea()
@@ -202,7 +219,8 @@ class TrackListing(QtWidgets.QSplitter):
         scroller.setWidget(self.track_editor)
         scroller.setWidgetResizable(True)
         # scroller.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        scroller.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        scroller.setSizeAdjustPolicy(
+            QtWidgets.QAbstractScrollArea.AdjustToContents)
         self.addWidget(scroller)
 
         self.track_listing.currentRowChanged.connect(self.set_row)
@@ -216,19 +234,24 @@ class TrackListing(QtWidgets.QSplitter):
 
         self.reset()
 
+    @staticmethod
+    def display_name(track):
+        """ Get the list display name for a track object """
+        parts = []
+        if 'filename' in track:
+            parts.append(os.path.basename(track['filename']))
+        if 'title' in track:
+            parts.append(track['title'])
+
+        return ': '.join(parts) if parts else '(unknown)'
+
     def reset(self):
         """ Reset to the backing storage """
         current_row = self.track_listing.currentRow()
         self.track_listing.clear()
 
         for track in self.data:
-            parts = []
-            if 'filename' in track:
-                parts.append(track['filename'])
-            if 'title' in track:
-                parts.append(track['title'])
-
-            self.track_listing.addItem(': '.join(parts) if parts else '(unknown)')
+            self.track_listing.addItem(self.display_name(track))
 
         if current_row < self.count():
             self.track_listing.setCurrentRow(current_row)
@@ -242,9 +265,33 @@ class TrackListing(QtWidgets.QSplitter):
         self.track_editor.apply()
         self.track_editor.reset(self.data[row])
 
+    def add_tracks(self):
+        """ Add some tracks """
+        filenames, _ = QtWidgets.QFileDialog.getOpenFileNames(self,
+                                                              "Select audio files",
+                                                              filter="WAV audio (*.wav)")
+
+        for filename in filenames:
+            _, title = util.guess_track_title(filename)
+            track = {'filename': filename, 'title': title}
+            self.data.append(track)
+            self.track_listing.addItem(self.display_name(track))
+
+    def delete_track(self):
+        """ Remove a track """
+        current_row = self.track_listing.currentRow()
+        del self.data[current_row]
+        self.track_listing.takeItem(current_row)
+        self.apply()
+
+    def move_up(self):
+        pass
+
+    def move_down(self):
+        pass
 
 
-class AlbumEditor(QtWidgets.QWidget):
+class AlbumEditor(QtWidgets.QMainWindow):
     """ An album editor window """
 
     def __init__(self, path: str):
@@ -255,15 +302,32 @@ class AlbumEditor(QtWidgets.QWidget):
         super().__init__()
         self.setMinimumSize(600, 0)
 
+        menubar = self.menuBar()
+
+        file_menu = menubar.addMenu("File")
+        file_menu.addAction(QtGui.QAction(
+            "&New...", shortcut=QtGui.QKeySequence("Ctrl+N"), triggered=self.file_new))
+        file_menu.addAction(QtGui.QAction(
+            "&Open...", shortcut=QtGui.QKeySequence("Ctrl+O"), triggered=self.file_open))
+        file_menu.addAction(QtGui.QAction(
+            "&Save", shortcut=QtGui.QKeySequence("Ctrl+S"), triggered=self.save))
+        file_menu.addAction(QtGui.QAction("Save &As...", shortcut=QtGui.QKeySequence(
+            "Ctrl+Shift+S"), triggered=self.save_as))
+
         self.filename = path
         self.data: typing.Dict[str, typing.Any] = {'tracks': []}
-        if os.path.isfile(path):
-            with open(path, 'r', encoding='utf8') as file:
-                self.data = json.load(file)
+        try:
+            self.reload(path)
+            self.data['tracks']
+        except (json.decoder.JSONDecodeError, KeyError, TypeError):
+            err = QtWidgets.QErrorMessage(self)
+            err.showMessage("Invalid album JSON file")
+            self.filename = ''
+            self.data = {'tracks': []}
 
         layout = QtWidgets.QFormLayout(
             fieldGrowthPolicy=QtWidgets.QFormLayout.AllNonFixedFieldsGrow)
-        self.setLayout(layout)
+        self.setCentralWidget(QtWidgets.QWidget(layout=layout))
 
         self.artist = QtWidgets.QLineEdit(placeholderText="Artist name")
         self.title = QtWidgets.QLineEdit(placeholderText="Album title")
@@ -292,12 +356,9 @@ class AlbumEditor(QtWidgets.QWidget):
 
         buttons = QtWidgets.QHBoxLayout()
 
-        save_button = QtWidgets.QPushButton("Save")
-        save_button.clicked.connect(self.apply)
         start_button = QtWidgets.QPushButton("Encode")
         start_button.clicked.connect(self.encode_album)
 
-        buttons.addWidget(save_button)
         buttons.addWidget(start_button)
 
         layout.addRow(buttons)
@@ -305,6 +366,30 @@ class AlbumEditor(QtWidgets.QWidget):
         self.setWindowTitle(self.filename)
 
         self.reset()
+
+    def file_new(self):
+        """ Dialog box to create a new album file """
+        path, _ = QtWidgets.QFileDialog.getSaveFileName(
+            caption="New album file",
+            filter="Album files (*.json *.bcalbum)")
+        if path:
+            editor = AlbumEditor(path)
+            editor.show()
+
+    def file_open(self):
+        """ Dialog box to open an existing file """
+        path, _ = QtWidgets.QFileDialog.getOpenFileName(
+            caption="New album file",
+            filter="Album files (*.json *.bcalbum)")
+        if path:
+            editor = AlbumEditor(path)
+            editor.show()
+
+    def reload(self, path):
+        """ Load from the backing storage """
+        if os.path.isfile(path):
+            with open(path, 'r', encoding='utf8') as file:
+                self.data = json.load(file)
 
     def reset(self):
         """ Reset to the saved values """
@@ -325,7 +410,7 @@ class AlbumEditor(QtWidgets.QWidget):
 
     def apply(self):
         """ Apply edits to the saved data """
-        relpath = FileSelector.make_relative(self.filename)
+        relpath = util.make_relative_path(self.filename)
 
         for key, widget in (
             ('title', self.title),
@@ -355,13 +440,58 @@ class AlbumEditor(QtWidgets.QWidget):
 
     def save(self):
         """ Save the file to disk """
+        if not self.filename:
+            self.save_as()
+
         with open(self.filename, 'w', encoding='utf8') as file:
             json.dump(self.data, indent=3)
+
+    def save_as(self):
+        """ Save the file and change the name """
+        path, _ = QtWidgets.QFileDialog.getSaveFileName(
+            caption="Select your album file",
+            filter="Album files (*.json *.bcalbum)",
+            options=QtWidgets.QFileDialog.DontConfirmOverwrite | QtWidgets.QFileDialog.DontUseNativeDialog)
+        if path:
+            self.setWindowTitle(self.filename)
+            self.renormalize_paths(self.filename, path)
+            self.filename = path
+            self.save()
+
+    def revert(self):
+        """ Revert all changes """
+        # TODO confirmation box
+        self.reload()
 
     def encode_album(self):
         """ Run the encoder process """
         self.apply()
         # TODO run the actual encode of course
+
+    def renormalize_paths(self, old_name, new_name):
+        """ Renormalize the file paths in the backing data """
+        abspath = util.make_relative_path(old_name)
+        relpath = util.make_relative_path(new_name)
+
+        def renorm(path):
+            return relpath(abspath(path))
+
+        for key in ('artwork',):
+            if key in self.data:
+                self.data[key] = renorm(self.data[key])
+
+        for track in self.data['tracks']:
+            for key in ('filename', 'artwork'):
+                if key in track:
+                    track[key] = renorm(track[key])
+            if 'lyrics' in track and isinstance(track['lyrics'], str) and os.path.isfile(abspath(track['lyrics'])):
+                track['lyrics'] = renorm(track['lyrics'])
+
+
+def open_file(path):
+    """ Open a file for editing """
+    editor = AlbumEditor(path)
+    editor.show()
 
 
 def main():
@@ -385,10 +515,14 @@ def main():
 
     if options.open_files:
         for path in options.open_files:
-            editor = AlbumEditor(path)
-            editor.show()
-    # else:
-    #   open file picker that opens an AlbumEditor with new or existing path
+            open_file(path)
+    else:
+        path, _ = QtWidgets.QFileDialog.getSaveFileName(
+            caption="Select your album file",
+            filter="Album files (*.json *.bcalbum)",
+            options=QtWidgets.QFileDialog.DontConfirmOverwrite | QtWidgets.QFileDialog.DontUseNativeDialog)
+        editor = AlbumEditor(path)
+        editor.show()
 
     app.exec()
 
