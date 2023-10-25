ab0d83db1ff8df72398222c74b1521516d9fe480
fluffy <fluffy@beesbuzz.biz>
2023-10-24 17:22:04 -0700

    Use devpi instance for universal2 wheel

--- make-universal2.sh
+++ make-universal2.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 # Generate the latest local universal2 wheels
 
-mkdir -p build
-cd build
+mkdir -p build/fuse-universal2
+cd build/fuse-universal2
+
+poetry run devpi use https://m.devpi.net/fluffy/prod
+poetry run devpi login fluffy
 
 for pkg in Pillow ; do
-    rm $pkg-*.whl
+    poetry run devpi use pypi
+    rm -f $pkg-*.whl
     for platform in macosx_10_10_x86_64 macosx_11_0_arm64 ; do
         poetry run pip download --only-binary=:all: --platform $platform $pkg
     done
     mkdir -p tmp
     poetry run delocate-fuse -w tmp  $pkg-*.whl
-    outfile=$(basename $pkg-*_arm64.whl arm64.whl)universal2.whl
+    outfile=../dist/$(basename $pkg-*_arm64.whl arm64.whl)universal2.whl
     mv tmp/$pkg-*.whl $outfile
+
+    poetry run devpi use fluffy/prod
+    poetry run devpi upload --formats=bdist_wheel $outfile
 done
-
-
