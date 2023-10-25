a0437b197302d7c54707087673318744b366239c
fluffy <fluffy@beesbuzz.biz>
2023-10-25 03:01:36 -0700

    Use the rtdio theme on rtd but not on local builds

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,3 @@ autoclass_content = "both"
 autodoc_member_order = "groupwise"
 autodoc_inherit_docstrings = False
 html_logo = "../art/bclogo.svg"
-
-# html_theme = 'sphinx_rtd_theme'
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
