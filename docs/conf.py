c0b74bd38ed23c986a666593e5fd8db8a3a4370e
fluffy <fluffy@beesbuzz.biz>
2023-10-27 02:11:30 -0700

    Attempt to get rtfd to see the actual library exports

--- docs/conf.py
+++ docs/conf.py
@@ -8,5 +8,5 @@ autodoc_member_order = "groupwise"
 autodoc_inherit_docstrings = False
 html_logo = "../art/bclogo.svg"
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = 'sphinx_rtd_theme'
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
