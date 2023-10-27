3707452e53e4c4ca0db862f08d501ebd73df07ac
fluffy <fluffy@beesbuzz.biz>
2023-10-27 02:45:39 -0700

    Make __version__ available to non-Makefile builds

--- get-version.sh
+++ get-version.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 # Set the current project version in __version__.py
-# There is probably a clever way to have Poetry do this but the documentation
-# runs me around in circles.
 
+mkdir -p build
 version=$(poetry version -s)
+
+cat > bandcrash/__version__.py << EOF
+""" Version information
+
+This one might be outdated but exists for the sake of builds that don't run
+through Make (e.g. readthedocs)
+"""
+
+try:
+    from .__build_version__ import __version__
+except ImportError:
+    __version__ = "$version"
+EOF
 
 if (git status --porcelain | grep -q .) ; then
     # Unchecked-in changes, so there's local edits
@@ -16,11 +28,11 @@ else
     tag=$(git rev-parse --short HEAD)
 fi
 
-echo "$version-$tag"
-
-cat > bandcrash/__version__.py << EOF
-""" Version information """
+cat > bandcrash/__build_version__.py << EOF
+""" Current git-based version """
 
 __version__ = "$version-$tag"
 EOF
+
+echo "$version-$tag"
 
