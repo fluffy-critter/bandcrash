6e63aa808b12246ed2e981c0076c2c3397cbb8f3
fluffy <fluffy@beesbuzz.biz>
2023-10-27 02:16:14 -0700

    Attempt to get rtfd to see the actual library exports

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,3 @@
-import sphinx_rtd_theme
-
 project = "bandcrash"
 master_doc = "index"
 extensions = ["sphinx.ext.autodoc"]
@@ -8,5 +6,6 @@ autodoc_member_order = "groupwise"
 autodoc_inherit_docstrings = False
 html_logo = "../art/bclogo.svg"
 
+# import sphinx_rtd_theme
 # html_theme = 'sphinx_rtd_theme'
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
