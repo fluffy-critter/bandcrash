1d8c3496bedecd9e160af69409c1371d7f9b1afe
fluffy <fluffy@beesbuzz.biz>
2023-10-31 13:59:05 -0700

    Add copy button to code snippets

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 project = "bandcrash"
 master_doc = "index"
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx_copybutton"]
 autoclass_content = "both"
 autodoc_member_order = "groupwise"
 autodoc_inherit_docstrings = False
